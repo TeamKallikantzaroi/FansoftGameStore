30db753484e604981578e2e3d468764cab945248
RosenUrkov <rosen.urkov@gmail.com>
2017-04-26 10:29:55 +0300

    Adding testing dependencies

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const express = require('express'),
     port = 1234;
 
 app.use(express.static('public'));
-app.use('/libs', express.static('node_modules'));
+app.use('/node_modules', express.static('node_modules'));
 
 app.listen(port, () => console.log(`Server running at http://localhost:${port}...`));
