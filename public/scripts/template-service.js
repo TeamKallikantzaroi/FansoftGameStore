b8e3334ca04aeeb565e26145d7abfa0e33c3d80b
RosenUrkov <rosen.urkov@gmail.com>
2017-04-27 11:05:08 +0300

    Placing main html into index.html

--- public/scripts/template-service.js
+++ public/scripts/template-service.js
@@ -3,20 +3,14 @@ import { requester } from 'requester';
 class TemplateService {
     constructor(requester) {
         this.requester = requester;
-        this._loadMainTemplate();
         this.cache = {};
     }
 
-    _loadMainTemplate() {
-        this.requester.getTemplate('main')
-            .then(template => $('.container').html(template));
-    }
-
     loadTemplate(name) {
-        if (this.cache[name]) {
-            $('#content').html(this.cache[name]);
-            return;
-        }
+        // if (this.cache[name]) {
+        //     $('#content').html(this.cache[name]);
+        //     return;
+        // }
 
         this.requester.getTemplate(name)
             .then((template) => this.cache[name] = template)
